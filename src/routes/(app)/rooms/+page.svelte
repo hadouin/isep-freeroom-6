15cf9b5dec970c13b78b96ac4b29574b489559e4
HLY5 <hadouin.leroy@3ds.com>
2024-04-02 10:26:27 +0200

    feat: :sparkles: add breadcrumbs

--- src/routes/(app)/rooms/+page.svelte
+++ src/routes/(app)/rooms/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import { RoomCard } from '$lib/components/rooms';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 
 	export let data;
 </script>
 
 <main class="flex flex-col flex-1 gap-4 p-4 overflow-scroll md:gap-8 md:p-8">
+	<Breadcrumb.Root>
+		<Breadcrumb.List>
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator />
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/rooms">Salles</Breadcrumb.Link>
+			</Breadcrumb.Item>
+		</Breadcrumb.List>
+	</Breadcrumb.Root>
+
 	{#each data.rooms as room}
 		<RoomCard roomId={room.id} />
 	{/each}
--- src/routes/(app)/rooms/[roomID]/+page.svelte
+++ src/routes/(app)/rooms/[roomID]/+page.svelte
@@ -18,9 +18,28 @@
 			throw new Error(res.status + res.statusText);
 		}
 	})(data.roomID);
+
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 </script>
 
 <main class="flex flex-col flex-1 gap-4 p-4 overflow-scroll md:gap-8 md:p-8">
+	<Breadcrumb.Root>
+		<Breadcrumb.List>
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator />
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/rooms">Salles</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator />
+			<Breadcrumb.Item>
+				<Breadcrumb.Page>{data.roomID}</Breadcrumb.Page>
+			</Breadcrumb.Item>
+		</Breadcrumb.List>
+	</Breadcrumb.Root>
+
 	{#await roomPromise}
 		<p>...waiting</p>
 	{:then response}
