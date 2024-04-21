7b2f1994c722db8a0a0f459dc7fd2572fb8f4423
Benjamin Bordes <benjaminbordes78@gmail.com>
2024-04-21 11:44:47 +0200

    feat default title with page specific optional tail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-	import '../app.pcss';
+	import { ModeWatcher } from "mode-watcher";
+	import { page } from '$app/stores'
+	import '../app.pcss'
+	const title = $page.data?.title
 </script>
 
+<svelte:head>
+	<title>FreeRoom{title ? ` • ${title}` : ''}</title>
+</svelte:head>
+
+<ModeWatcher />
 <slot />
